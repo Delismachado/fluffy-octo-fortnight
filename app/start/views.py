27318cfc19e9656f2550c1cd6ca52e3bf95582db
Derli Machado <dam.machado1@gmail.com>
2021-02-12 06:46:06 -0300

    Added: urls to users and grupos.

--- app/start/views.py
+++ app/start/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from app.start.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all().order_by('-date_joined')
+  serializer_class = UserSerializer
+  permissions_class = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+  queryset = Group.objects.all()
+  serializer_class = GroupSerializer
+  permissions_class = [permissions.IsAuthenticated]
