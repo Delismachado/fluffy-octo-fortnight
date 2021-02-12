f62d3ec8c3c468a04faf2a3921746c0977a47fdc
Derli Machado <dam.machado1@gmail.com>
2021-02-12 06:48:08 -0300

    fix: formatting for pep8.

--- app/start/views.py
+++ app/start/views.py
@@ -5,12 +5,12 @@ from app.start.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all().order_by('-date_joined')
-  serializer_class = UserSerializer
-  permissions_class = [permissions.IsAuthenticated]
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permissions_class = [permissions.IsAuthenticated]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-  queryset = Group.objects.all()
-  serializer_class = GroupSerializer
-  permissions_class = [permissions.IsAuthenticated]
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permissions_class = [permissions.IsAuthenticated]
